c72d4f9c66ff5071542be265e5fb5aed66deb0b5
abhinavsinhak <shuhantgaurav2005@gmail.com>
2023-02-20 23:39:24 +0530

    hello

--- map.java
+++ map.java
@@ -1,7 +1,14 @@
 import java.util.*;
-
 public class map{
     public static void main(String[] args) {
+        HashMap<String,Integer> scr = new HashMap<>();
+          
+        scr.put("sushant", 1000);
+        scr.put("gaurav", 500);
+        scr.put("aashu",2000);
+        
+        System.out.println(scr);
+                
         
     }
 }
