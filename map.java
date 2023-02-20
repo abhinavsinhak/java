2d10eb3a783a6dffd22a8a01cc62e9454df98630
abhinavsinhak <shuhantgaurav2005@gmail.com>
2023-02-20 23:56:57 +0530

    hello

--- map.java
+++ map.java
@@ -7,8 +7,18 @@ public class map{
         scr.put("gaurav", 500);
         scr.put("aashu",2000);
         
-        System.out.println(scr);
-                
+        // System.out.println(scr);
+        // System.out.println(scr.containsKey("sushant"));
+        // System.out.println(scr.get("aashu"));
+
+        for(Map.Entry<String,Integer> it :scr.entrySet()){
+            System.out.println(it.getValue());
+        }
+
+        Set<String> keys = scr.keySet();
+        for(String key:keys){
+            System.out.println(key+" "+scr.get(key));
+        }
         
     }
 }
